2464b2cf490dbace74b7ab7c8fa9a47dd8ffe109
Huy Dang <hqdang97@gmail.com>
2019-10-14 22:05:40 -0600

    The app now is able to download doc and save signatures.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,117 @@
-import React, { useState } from "react";
+import React from "react";
 import { Button } from "semantic-ui-react";
 import SigningPad from "./components/SigningPad";
 import Dropzone from "react-dropzone";
 import PDFJS from "pdfjs-dist";
+import jsPDF from "jspdf";
 
 import "./App.css";
 
-function App() {
-  const [showSigning, setShowSigning] = useState(false);
-  PDFJS.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${PDFJS.version}/pdf.worker.js`;
-
-  const handleDrop = acceptedFiles => {
-    loadPDFtoCanvas(acceptedFiles[0]);
+class App extends React.Component {
+  state = {
+    showSigning: false,
+    placeholders: [],
+    signatures: []
   };
 
-  const loadPDFtoCanvas = file => {
+  componentDidMount() {
+    PDFJS.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${PDFJS.version}/pdf.worker.js`;
+  }
+
+  packAllPagesToDownload = () => {
+    const finalPDF = new jsPDF();
+    const canvasContainer = document.getElementById("canvas-container");
+    canvasContainer.childNodes.forEach((child, index) => {
+      const imgData = child.toDataURL("image/jpeg", 1.0);
+      if (index === 0) {
+        finalPDF.addImage(imgData, "JPEG", 0, 0);
+      } else {
+        const newPage = finalPDF.addPage();
+        newPage.addImage(imgData, "JPEG", 0, 0);
+      }
+    });
+    finalPDF.save("download.pdf");
+  };
+
+  generatePlaceholder = (mousePos, canvas) => {
+    const { placeholders } = this.state;
+    const ctx = canvas.getContext("2d");
+    const imgData = ctx.getImageData(mousePos.x, mousePos.y, 100, 50);
+    ctx.rect(mousePos.x, mousePos.y, 100, 50);
+    ctx.fillStyle = "yellow";
+    ctx.fill();
+
+    this.setState({
+      placeholders: [...placeholders, { canvas, mousePos, imgData }]
+    });
+  };
+
+  canvasUpdated = pageNumbers => {
+    const canvasContainer = document.getElementById("canvas-container");
+    if (pageNumbers === canvasContainer.childNodes.length) {
+      function getMousePos(canvas, evt) {
+        const rect = canvas.getBoundingClientRect();
+        return {
+          x: evt.clientX - rect.left - 20,
+          y: evt.clientY - rect.top - 20
+        };
+      }
+
+      canvasContainer.childNodes.forEach(child => {
+        child.addEventListener(
+          "click",
+          evt => {
+            const mousePos = getMousePos(child, evt);
+            this.generatePlaceholder(mousePos, child);
+          },
+          false
+        );
+      });
+    }
+  };
+
+  onSubmitSig = url => {
+    this.setState({ signatures: [...this.state.signatures, url] }, () => {
+      const lastIndexSig = this.state.signatures.length - 1;
+      this.addSignature(lastIndexSig);
+    });
+  };
+
+  addSignature = index => {
+    const placeholder = this.state.placeholders[index];
+    const signature = this.state.signatures[index];
+
+    const pdf = placeholder.canvas.getContext("2d");
+    // clear the yellow box
+    pdf.clearRect(placeholder.mousePos.x, placeholder.mousePos.y, 100, 50);
+    pdf.putImageData(
+      placeholder.imgData,
+      placeholder.mousePos.x,
+      placeholder.mousePos.y
+    );
+
+    // add signature
+    const img = document.createElement("img");
+    img.setAttribute("src", signature);
+    img.setAttribute("style", { display: "none" });
+    document.getElementById("App").appendChild(img);
+
+    img.onload = () => {
+      pdf.drawImage(
+        img,
+        placeholder.mousePos.x,
+        placeholder.mousePos.y,
+        100,
+        100
+      );
+    };
+  };
+
+  handleDrop = acceptedFiles => {
+    this.loadPDFtoCanvas(acceptedFiles[0], this);
+  };
+
+  loadPDFtoCanvas = (file, componentContext) => {
     if (file.type !== "application/pdf") {
       console.error(file.name, "is not a pdf file.");
       return;
@@ -24,14 +121,14 @@ function App() {
 
     fileReader.onload = function() {
       const typedArray = new Uint8Array(this.result);
-
-      PDFJS.getDocument(typedArray).then(function(pdf) {
+      PDFJS.getDocument(typedArray).then(pdf => {
         // you can now use *pdf* here
         const renderPage = async pageNum => {
           try {
             const page = await pdf.getPage(pageNum);
             const viewport = page.getViewport(1);
             const canvas = document.createElement("canvas");
+            canvas.setAttribute("id", `page-${pageNum}`);
             const canvasContainer = document.getElementById("canvas-container");
             const ctx = canvas.getContext("2d");
             const renderContext = {
@@ -42,50 +139,55 @@ function App() {
             canvas.height = viewport.height;
             canvas.width = viewport.width;
             canvasContainer.appendChild(canvas);
-
             page.render(renderContext);
+            componentContext.canvasUpdated(pdf.numPages);
           } catch (err) {
             console.log("ERROR", err);
           }
         };
 
         const pageNumbers = [...Array(pdf.numPages).keys()]; // Produce array [0,1,2,...]
-
-        console.log("pageNumbers", pageNumbers);
-        pageNumbers.forEach(pageNum => {
-          console.log("check", pageNum + 1);
-          renderPage(pageNum + 1);
-        });
+        pageNumbers.forEach(pageNum => renderPage(pageNum + 1));
       });
     };
     PDFJS.disableWorker = true;
     fileReader.readAsArrayBuffer(file);
   };
 
-  return (
-    <div className="App">
-      <br />
-      <Dropzone onDrop={handleDrop}>
-        {({ getRootProps, getInputProps }) => (
-          <section>
-            <div {...getRootProps()}>
-              <input {...getInputProps()} />
-              <Button color="blue">Upload</Button>
-            </div>
-          </section>
-        )}
-      </Dropzone>
+  render() {
+    const { showSigning, placeholders, signatures } = this.state;
+    return (
+      <div className="App" id="App">
+        <br />
+        <Dropzone onDrop={this.handleDrop}>
+          {({ getRootProps, getInputProps }) => (
+            <section>
+              <div {...getRootProps()}>
+                <input {...getInputProps()} />
+                <Button color="blue">Upload</Button>
+              </div>
+            </section>
+          )}
+        </Dropzone>
 
-      <Button
-        onClick={() => setShowSigning(!showSigning)}
-        style={{ margin: "20px 0px" }}
-      >
-        Show Signing Pad
-      </Button>
-      {showSigning && <SigningPad />}
-      <div id="canvas-container"></div>
-    </div>
-  );
+        <Button
+          onClick={() => this.setState({ showSigning: !showSigning })}
+          style={{ margin: "20px 0px" }}
+        >
+          Show Signing Pad
+        </Button>
+        <Button onClick={this.packAllPagesToDownload}>Download</Button>
+        {showSigning && (
+          <SigningPad
+            onSubmit={this.onSubmitSig}
+            totalPlaceHolders={placeholders.length}
+            totalSignatures={signatures.length}
+          />
+        )}
+        <div id="canvas-container"></div>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/SigningPad.js
+++ src/components/SigningPad.js
@@ -1,15 +1,50 @@
 import React, { useRef } from "react";
 import SignatureCanvas from "react-signature-canvas";
-import { Button } from "semantic-ui-react";
+import { Button, Message, Progress } from "semantic-ui-react";
 
-export default function SigningPad({ onSubmit }) {
+export default function SigningPad({
+  onSubmit,
+  totalPlaceHolders,
+  totalSignatures
+}) {
   const canvasRef = useRef(null);
   const onSubmitSignature = () => {
-    onSubmit(canvasRef.current.toDataURL("image/jpeg", 1.0));
+    onSubmit(canvasRef.current.toDataURL("image/png", 1.0));
     canvasRef.current.clear();
   };
+
+  const diff = totalPlaceHolders - totalSignatures;
+  const progressState = {
+    waiting: {
+      processBarMes: "Almost Done",
+      messageState: {
+        warning: true,
+        header: `You have ${
+          diff > 1 ? `${diff} signatures` : `${diff} signature`
+        } left`
+      }
+    },
+    success: {
+      processBar: "Complete",
+      messageState: {
+        positive: true,
+        header: `You finished`
+      }
+    }
+  };
+
+  const currentState = diff === 0 ? "success" : "waiting";
+
   return (
     <>
+      <Progress
+        value={totalSignatures}
+        total={totalPlaceHolders}
+        progress="ratio"
+      >
+        {progressState[currentState].processBarMes}
+      </Progress>
+      <Message {...progressState[currentState].messageState} />
       <div className="signing-pad">
         <SignatureCanvas
           ref={canvasRef}
@@ -18,10 +53,20 @@ export default function SigningPad({ onSubmit }) {
         />
       </div>
       <br />
-      <Button inverted color="green" onClick={onSubmitSignature}>
+      <Button
+        inverted
+        color="green"
+        onClick={onSubmitSignature}
+        style={{ zIndex: "100" }}
+      >
         Submit
       </Button>
-      <Button inverted color="blue" onClick={() => canvasRef.current.clear()}>
+      <Button
+        inverted
+        color="blue"
+        onClick={() => canvasRef.current.clear()}
+        style={{ zIndex: "100" }}
+      >
         Clear
       </Button>
     </>
